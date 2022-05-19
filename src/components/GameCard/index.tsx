007feb571863efe2e438625e551b16d12414d8e4
VJS <minsung_96@naver.com>
2022-05-19 23:56:02 +0900

    Icons

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,14 +1,31 @@
 import React, { ReactElement } from 'react'
 import { Game } from 'types'
 import { StyledLink, Details, Img, Title, Description, Genre } from './styles'
+import windowsIcon from 'assets/icons/windows.svg'
+import browserIcon from 'assets/icons/browser.svg'
+import { BROWSER, WINDOWS } from './constants'
 
 interface Props {
     content: Game
 }
 
 const GameCard = ({ content } : Props): ReactElement => {
-    const {id, title, thumbnail, short_description, genre } = content
+    const {id, title, thumbnail, short_description, genre, platform } = content
     const link = `/game/${id}`
+    const icons = platform.split(',').map(p=> {
+        let icon = null
+        switch(p.trim()) {
+            case BROWSER:
+                icon = <img key={`${id}-browser`} alt="browser icon" src={browserIcon}/>
+                break
+            case WINDOWS:
+                icon = <img key={`${id}-windows`} alt="Windows icon" src={windowsIcon}/>
+                break
+            default:
+                break
+        }
+        return icon
+    })
 
     return (
         <StyledLink to={link}>
@@ -23,6 +40,7 @@ const GameCard = ({ content } : Props): ReactElement => {
                 <Genre>
                     {genre}
                 </Genre>
+                {icons}
             </Details>
 
         </StyledLink>
